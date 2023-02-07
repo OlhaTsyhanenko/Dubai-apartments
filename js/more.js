6b2cf887ccc175ecf75f565d9335a89736d86339
Olha Tsyhanenko <olya.tsyganenko@gmail.com>
2023-02-07 21:53:42 +0200

    fix button

--- js/more.js
+++ js/more.js
@@ -3,14 +3,34 @@ let p = document.getElementsByClassName('descr');
 
 for (let i = 0; i < btn.length; i++) {
     btn[i].addEventListener('click', function () {
-        let content = p[i];
-        if (content.classList.contains('hidden')) {
-            content.classList.remove('hidden');
-            this.innerHTML = 'Less';
-        } else {
-            content.classList.add('hidden');
-            this.innerHTML = 'Read more';
+        if (p[i]) {
+            let content = p[i];
+            if (content.classList.contains('hidden')) {
+                content.classList.remove('hidden');
+            } else {
+                content.classList.add('hidden');
+            }
         }
     });
 }
 
+let change = document.querySelectorAll('.change');
+let imgChecked = document.querySelectorAll('.checked');
+let imgNotChecked = document.querySelectorAll('.not-checked');
+
+for (let i = 0; i < change.length; i++) {
+    if (p[i]) {
+        const showCheckbox = () => {
+
+            imgChecked[i].classList.toggle('visible');
+            imgNotChecked[i].classList.toggle('visible');
+            if (!change[i].checked) {
+                change[i].checked = true;
+            } else {
+                change[i].checked = false;
+            }
+        }
+        imgChecked[i].addEventListener('click', showCheckbox);
+        imgNotChecked[i].addEventListener('click', showCheckbox)
+    }
+}
